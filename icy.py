e5438b6054084333f90689aa2a0cfc9050a5efae
Rainbow: the Mad Faerie <rtmf@beautifulsunrise.org>
2017-03-18 02:13:43 -0700

    add support for multiple IRC servers!

--- icy.py
+++ icy.py
@@ -10,7 +10,7 @@ import importlib
 
 	
 def printTitles():
-	global icy,bot,cfg,ttl
+	global icy,bot,cfg,ttl,bth
 	try:
 		titles=icy.title()
 		if len(titles)==0:
@@ -23,15 +23,33 @@ def printTitles():
 		ttl=threading.Timer(1,printTitles)
 		ttl.start()
 
+def runBot(ircbot):
+	try:
+		print(ircbot._host)
+		ircbot.start()
+	except:
+		reload_func()
+
 def reload_func(cmdonly=0):
-	global icy,bot,cfg,ttl
+	global icy,bot,cfg,ttl,bth
 	if (cmdonly==1):
 		importlib.reload(icybot_cmd)
 		bot._cmd=icybot_cmd.IcyBotCommands(bot,reload_func)
 	else:
 		if bot is not None:
-			bot.disconnect()
+			for ircbot in bot:
+				try:
+					ircbot.disconnect()
+					del ircbot
+				except:
+					pass
+			for bthread in bth:
+				try:
+					del bthread
+				except:
+					pass
 		del bot
+		del bth
 		del icy
 		del cfg
 		del ttl
@@ -41,19 +59,20 @@ def reload_func(cmdonly=0):
 		importlib.reload(icybot_cfg)
 		cfg = s2x(open('icy.cfg').read())
 		icy = icybot_icy.Icecast(cfg.find("icecast"))
-		bot = icybot_bot.RFHBot(icy,cfg.find("irc"),reload_func)
+		bot = [icybot_bot.RFHBot(icy,irccfg,reload_func) for irccfg in cfg.findall("irc")]
+		bth = [threading.Thread(target=runBot,args=(ircbot,)) for ircbot in bot]
+		for bthread in bth:
+			bthread.start()
 		printTitles()
-		try:
-			bot.start()
-		except:
-			reload_func()
+
 
 def main():
-	global icy,bot,cfg,ttl
+	global icy,bot,cfg,ttl,bth
 	cfg=None
 	icy=None
 	bot=None
 	ttl=None
+	bth=None
 	reload_func()
 
 
